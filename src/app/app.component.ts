d066955907f712edeee9f8fe75db5e2a8a69d0e0
rehabmohamed2 <88574414+rehabmohamed2@users.noreply.github.com>
2024-09-20 04:17:26 +0300

    Developed portfolio page using Angular with Bootstrap components.
    Revamped app.component.html and built separate Angular components for the portfolio sections: Hero (name and job title), Bio/About Me with a CV download button, Skills with dynamic progress bars, Portfolio/Projects showcase, and a Footer featuring contact information and social media links (Facebook, GitHub, LinkedIn) with FontAwesome icons. Implemented Bootstrap for responsive design and layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './component/about/about.component';
+import { HeroComponent } from './component/hero/hero.component';
+import { SkillsComponent } from './component/skills/skills.component';
+import { PortfolioComponent } from './component/portfolio/portfolio.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeroComponent,AboutComponent , SkillsComponent , PortfolioComponent , FooterComponent , FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
